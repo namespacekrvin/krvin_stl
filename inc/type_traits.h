a8ebb1d8c392d0ba321bb34bb355a1f0a2b9ede8
krvin <ssjsyh@mail.ustc.edu.cn>
2026-09-16 22:59:01 +0800

    [add] finish type_traits& test

---
+++ inc/type_traits.h
@@ -0,0 +1,171 @@
+#ifndef __TYPE_TRAITS_H__
+#define __TYPE_TRAITS_H__
+
+namespace KrvinSTL {
+  namespace {
+    template<bool, typename Ta, typename Tb>
+    struct IfThenElse;
+
+    template<typename Ta, typename Tb>
+    struct IfThenElse<true, Ta, Tb> {
+      using result = Ta;
+    };
+
+    template<typename Ta, typename Tb>
+    struct IfThenElse<false, Ta, Tb> {
+      using result = Tb;
+    };
+  }
+
+  struct _true_type {};
+  struct _false_type {};
+
+  template<typename T>
+  struct _type_traits {
+    typedef _false_type  has_trivial_default_constructor;
+    typedef _false_type		has_trivial_copy_constructor;
+		typedef _false_type		has_trivial_assignment_operator;
+		typedef _false_type		has_trivial_destructor;
+		typedef _false_type		is_POD_type;
+  };
+
+  template<>
+  struct _type_traits<bool> {
+    typedef _true_type		has_trivial_default_constructor;
+		typedef _true_type		has_trivial_copy_constructor;
+		typedef _true_type		has_trivial_assignment_operator;
+		typedef _true_type		has_trivial_destructor;
+		typedef _true_type		is_POD_type;
+  };
+
+  template<>
+  struct _type_traits<char> {
+    typedef _true_type		has_trivial_default_constructor;
+		typedef _true_type		has_trivial_copy_constructor;
+		typedef _true_type		has_trivial_assignment_operator;
+		typedef _true_type		has_trivial_destructor;
+		typedef _true_type		is_POD_type;
+  };
+
+  template<>
+  struct _type_traits<unsigned char> {
+    typedef _true_type		has_trivial_default_constructor;
+		typedef _true_type		has_trivial_copy_constructor;
+		typedef _true_type		has_trivial_assignment_operator;
+		typedef _true_type		has_trivial_destructor;
+		typedef _true_type		is_POD_type;
+  };
+
+  template<>
+  struct _type_traits<short> {
+    typedef _true_type		has_trivial_default_constructor;
+		typedef _true_type		has_trivial_copy_constructor;
+		typedef _true_type		has_trivial_assignment_operator;
+		typedef _true_type		has_trivial_destructor;
+		typedef _true_type		is_POD_type;
+  };
+
+  template<>
+  struct _type_traits<unsigned short> {
+    typedef _true_type		has_trivial_default_constructor;
+		typedef _true_type		has_trivial_copy_constructor;
+		typedef _true_type		has_trivial_assignment_operator;
+		typedef _true_type		has_trivial_destructor;
+		typedef _true_type		is_POD_type;
+  };
+
+  template<>
+  struct _type_traits<int> {
+    typedef _true_type		has_trivial_default_constructor;
+		typedef _true_type		has_trivial_copy_constructor;
+		typedef _true_type		has_trivial_assignment_operator;
+		typedef _true_type		has_trivial_destructor;
+		typedef _true_type		is_POD_type;
+  };
+
+  template<>
+  struct _type_traits<unsigned int> {
+    typedef _true_type		has_trivial_default_constructor;
+		typedef _true_type		has_trivial_copy_constructor;
+		typedef _true_type		has_trivial_assignment_operator;
+		typedef _true_type		has_trivial_destructor;
+		typedef _true_type		is_POD_type;
+  };
+
+  template<>
+  struct _type_traits<long> {
+    typedef _true_type		has_trivial_default_constructor;
+		typedef _true_type		has_trivial_copy_constructor;
+		typedef _true_type		has_trivial_assignment_operator;
+		typedef _true_type		has_trivial_destructor;
+		typedef _true_type		is_POD_type;
+  };
+
+  template<>
+  struct _type_traits<unsigned long> {
+    typedef _true_type		has_trivial_default_constructor;
+		typedef _true_type		has_trivial_copy_constructor;
+		typedef _true_type		has_trivial_assignment_operator;
+		typedef _true_type		has_trivial_destructor;
+		typedef _true_type		is_POD_type;
+  };
+
+  template<>
+  struct _type_traits<long long> {
+    typedef _true_type		has_trivial_default_constructor;
+		typedef _true_type		has_trivial_copy_constructor;
+		typedef _true_type		has_trivial_assignment_operator;
+		typedef _true_type		has_trivial_destructor;
+		typedef _true_type		is_POD_type;
+  };
+
+  template<>
+  struct _type_traits<unsigned long long> {
+    typedef _true_type		has_trivial_default_constructor;
+		typedef _true_type		has_trivial_copy_constructor;
+		typedef _true_type		has_trivial_assignment_operator;
+		typedef _true_type		has_trivial_destructor;
+		typedef _true_type		is_POD_type;
+  };
+
+  template<>
+  struct _type_traits<float> {
+    typedef _true_type		has_trivial_default_constructor;
+		typedef _true_type		has_trivial_copy_constructor;
+		typedef _true_type		has_trivial_assignment_operator;
+		typedef _true_type		has_trivial_destructor;
+		typedef _true_type		is_POD_type;
+  };
+
+  template<>
+  struct _type_traits<double> {
+    typedef _true_type		has_trivial_default_constructor;
+		typedef _true_type		has_trivial_copy_constructor;
+		typedef _true_type		has_trivial_assignment_operator;
+		typedef _true_type		has_trivial_destructor;
+		typedef _true_type		is_POD_type;
+  };
+
+  template<class T>
+	struct _type_traits<T*>
+	{
+		typedef _true_type		has_trivial_default_constructor;
+		typedef _true_type		has_trivial_copy_constructor;
+		typedef _true_type		has_trivial_assignment_operator;
+		typedef _true_type		has_trivial_destructor;
+		typedef _true_type		is_POD_type;
+	};
+
+  template<class T>
+	struct _type_traits<const T*>
+	{
+		typedef _true_type		has_trivial_default_constructor;
+		typedef _true_type		has_trivial_copy_constructor;
+		typedef _true_type		has_trivial_assignment_operator;
+		typedef _true_type		has_trivial_destructor;
+		typedef _true_type		is_POD_type;
+	};
+  
+}
+
+#endif
---
+++ test/type_traits_test.cpp
@@ -0,0 +1,60 @@
+#include "../inc/type_traits.h"
+#include <iostream>
+#include <cstring>
+#include <new>
+
+namespace KrvinSTL {
+  template<typename Input, typename Output> 
+  Output uninitialized_copy_impl(Input first, Input last, Output dest, _true_type) {
+    std::cout << "Use memcpy for pod type!" << std::endl;
+    std::memcpy(dest, first, (last - first) * sizeof(*first));
+    return dest + (last - first);
+  }
+
+  template<typename Input, typename Output> 
+  Output uninitialized_copy_impl(Input first, Input last, Output dest, _false_type) {
+    std::cout << "Use copy construct for unpod type!" << std::endl;
+    Output current = dest;
+    for (; first != last; ++first, ++current) {
+      new (static_cast<void*>(&(*current))) typename std::remove_reference<decltype(*first)>::type(*first);
+    }
+    return current;
+  }
+
+  template<typename Input, typename Output>
+  Output uninitialized_copy(Input first, Input last, Output dest) {
+    using value_type = typename std::remove_reference<decltype(*first)>::type;
+    using is_pod = typename _type_traits<value_type>::is_POD_type;
+
+    return uninitialized_copy_impl(first, last, dest, is_pod());
+  }
+}
+
+struct ComplexObj {
+  int *data;
+  ComplexObj() : data(new int(1)) {}
+  ComplexObj(const ComplexObj& obj) : data(new int(*obj.data)) {
+    std::cout << "ComplextObj call copy construct!" << std::endl;
+  }
+  ~ComplexObj() { delete data;}
+};
+
+int main() {
+  // 测试1：POD 类型（int 数组）
+  std::cout << "=== 测试 POD 类型 ===\n";
+  int src_int[] = {1, 2, 3, 4, 5};
+  int dest_int[5];
+  KrvinSTL::uninitialized_copy(src_int, src_int + 5, dest_int);
+  for (int v : dest_int) std::cout << v << " ";
+  std::cout << "\n\n";
+
+  // 测试2：非 POD 类型（自定义类数组）
+  std::cout << "=== 测试非 POD 类型 ===\n";
+  ComplexObj src_obj[3];
+  alignas(ComplexObj) char buffer[3 * sizeof(ComplexObj)];  // 裸内存
+  ComplexObj* dest_obj = reinterpret_cast<ComplexObj*>(buffer);
+  KrvinSTL::uninitialized_copy(src_obj, src_obj + 3, dest_obj);
+  std::cout << "拷贝完成，dest_obj[0].data = " << *dest_obj[0].data << "\n";
+
+  return 0;
+}
